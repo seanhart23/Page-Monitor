359b36d50a3b98598fccf8a41139aece6a9af153
Sean Hart <seanhart23@gmail.com>
2026-07-27 10:50:10 -0400

    added resuable header and footers

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -6,22 +6,49 @@ const PRIVACY_URL = "privacy.html";
 
 document.addEventListener("DOMContentLoaded", () => {
   const yearElement = document.getElementById("year");
+  const siteHeaderElement = document.querySelector(".site-header");
+  const siteFooterElement = document.querySelector(".site-footer");
+
+  function siteHeader() {
+    return `
+    <div class="container header-content">
+      <a class="brand" href="${BASE_URL}${INDEX_URL}" class="index-url">
+        <span class="brand-icon"><img src="./icons/icon.png"></span>
+        <span>Smart Page Monitor</span>
+      </a>
+
+      <nav class="navigation" aria-label="Main navigation">
+        <a href="${BASE_URL}${INDEX_URL}" class="index-url">Home</a>
+        <a href="${BASE_URL}${SUPPORT_URL}" class="support-url">Support</a>
+        <a href="${BASE_URL}${PRIVACY_URL}" class="privacy-url">Privacy</a>
+      </nav>
+    </div>
+  `;
+  }
+
+  function siteFooter() {
+    return `
+    <div class="container footer-content">
+      <p>
+        © <span id="current-year"></span>
+        Smart Page Monitor.
+      </p>
+
+      <div class="footer-links">
+        <a href="${BASE_URL}${PRIVACY_URL}" class="privacy-url">Privacy Policy</a>
+        <a href="${BASE_URL}${SUPPORT_URL}" class="support-url">Support</a>
+      </div>
+    </div>
+  `;
+  }
 
   if (yearElement) {
     yearElement.textContent =
       String(new Date().getFullYear());
   }
 
-  const getPageUrl = (className, base, url) => { 
-    const allIndexLinks = document.querySelectorAll(className);
-    allIndexLinks.forEach(link => {
-        link.href = base + url;
-    });
-  }
-
-  getPageUrl('.index-url', `${BASE_URL}`,`${INDEX_URL}` )
-  getPageUrl('.support-url', `${BASE_URL}`,`${SUPPORT_URL}` )
-  getPageUrl('.privacy-url', `${BASE_URL}`,`${PRIVACY_URL}` )
+  siteHeaderElement.innerHTML = siteHeader();
+  siteFooterElement.innerHTML = siteFooter();
 
 });
 
